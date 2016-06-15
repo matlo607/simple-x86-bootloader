d5538ee3de8f8ff1bb265ba726a6493762be5f1e
Matthieu Longo <mateo.longo@gmail.com>
2016-06-15 11:20:15 +0200

    New features on A20 gate
    Details:
        - disable a20 gate
        - get state of a20 gate
    
    Bug fix:
        - bad register when settings the bios call to enable the a20 gate in Real Mode

--- arch/x86/lib/a20.c
+++ arch/x86/lib/a20.c
@@ -23,7 +23,7 @@ int a20gate_enable(void)
 
     x86_regs_init(&regs_in);
 
-    regs_in.D._Rl = 0x1;
+    regs_in.A._Rl = 0x1;
     regs_in.A._Rh = 0x24;
 
     bioscall(0x15, &regs_in, &regs_out);
@@ -34,4 +34,74 @@ int a20gate_enable(void)
 #endif
 
     return status;
+}
+
+int a20gate_disable(void)
+{
+    int status = EXIT_SUCCESS;
+
+#ifdef BOOTLOADER_PROTECTED_MODE_ENABLED
+    regs16_t regs;
+
+    regs.a._Rl = 0x0;
+    regs.a._Rh = 0x24;
+
+    int32(0x15, &regs);
+
+    if (regs.eflags.CF) {
+        status = EXIT_FAILURE;
+    }
+#else
+    x86_regs_t regs_in, regs_out;
+
+    x86_regs_init(&regs_in);
+
+    regs_in.A._Rl = 0x0;
+    regs_in.A._Rh = 0x24;
+
+    bioscall(0x15, &regs_in, &regs_out);
+
+    if (regs_out.eflags.CF) {
+        status = EXIT_FAILURE;
+    }
+#endif
+
+    return status;
+}
+
+extern bool a20gate_enabled(void)
+{
+    bool enabled;
+
+#ifdef BOOTLOADER_PROTECTED_MODE_ENABLED
+    regs16_t regs;
+
+    regs.a._Rl = 0x2;
+    regs.a._Rh = 0x24;
+
+    int32(0x15, &regs);
+
+    if (regs.eflags.CF) {
+        enabled = false;
+    } else {
+        enabled = (regs.a._Rl == 0x1) ? true : false;
+    }
+#else
+    x86_regs_t regs_in, regs_out;
+
+    x86_regs_init(&regs_in);
+
+    regs_in.A._Rl = 0x2;
+    regs_in.A._Rh = 0x24;
+
+    bioscall(0x15, &regs_in, &regs_out);
+
+    if (regs_out.eflags.CF) {
+        enabled = false;
+    } else {
+        enabled = (regs_in.A._Rl == 0x1) ? true : false;
+    }
+#endif
+
+    return enabled;
 }
--- include/arch/x86/a20.h
+++ include/arch/x86/a20.h
@@ -1,6 +1,12 @@
 #ifndef _A20_H
 #define _A20_H
 
+#include <stdbool.h>
+
 extern int a20gate_enable(void);
+
+extern int a20gate_disable(void);
+
+extern bool a20gate_enabled(void);
 
 #endif
