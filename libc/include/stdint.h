c2a2854f36c2dfa095aa5db7457bbf5f5fd3f9b9
Matthieu Longo <mateo.longo@gmail.com>
2016-06-15 23:38:24 +0200

    Add limits in stdint.h

--- libc/include/stdint.h
+++ libc/include/stdint.h
@@ -11,4 +11,23 @@ typedef signed short        int16_t;
 typedef signed int          int32_t;
 typedef signed long long    int64_t;
 
+typedef int32_t             intptr_t;
+typedef uint32_t            uintptr_t;
+
+
+#define INT8_MIN            (-128)
+#define INT16_MIN           (-32768)
+#define INT32_MIN           (-2147483647 - 1)
+#define INT64_MIN           (-9223372036854775807LL - 1)
+
+#define INT8_MAX            127
+#define INT16_MAX           32767
+#define INT32_MAX           2147483647
+#define INT64_MAX           9223372036854775807LL
+
+#define UINT8_MAX           0xff /* 255U */
+#define UINT16_MAX          0xffff /* 65535U */
+#define UINT32_MAX          0xffffffff  /* 4294967295U */
+#define UINT64_MAX          0xffffffffffffffffULL /* 18446744073709551615ULL */
+
 #endif
